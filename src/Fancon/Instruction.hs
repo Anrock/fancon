27c3941656f589373e803ebf3f2cb54cab9c0035
Anrock <anrock623@gmail.com>
2020-04-18 17:49:39 +0300

    Add Fancon.Instruction.printInstructions

--- src/Fancon/Instruction.hs
+++ src/Fancon/Instruction.hs
@@ -4,8 +4,16 @@ module Fancon.Instruction
   , Instruction
   , opcode
   , operands
+  , printInstructions
   , module Fancon.Instruction.Validate
   ) where
 
 import Fancon.Instruction.Internal
 import Fancon.Instruction.Validate
+import Data.Array
+import Data.String.Interpolate
+import Data.List (intercalate)
+
+printInstructions :: Array Int Instruction -> String
+printInstructions instructions = [i|#{length instructions} instructions:\n#{intercalate "\n" $ show <$> assocs instructions}|]
+
