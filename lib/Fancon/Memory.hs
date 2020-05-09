49a4ac1d3195c965beffbf146fa62b83118d20db
Anrock <anrock623@gmail.com>
2020-05-09 17:48:51 +0300

    Add firstByte, secondByte to Fancon.Memory

--- lib/Fancon/Memory.hs
+++ lib/Fancon/Memory.hs
@@ -2,6 +2,8 @@ module Fancon.Memory
   ( Address
   , bytesToWord
   , wordToBytes
+  , firstByte
+  , secondByte
   , byteToNibbles
   ) where
 
@@ -16,6 +18,12 @@ bytesToWord hi lo = fromIntegral $ shiftL (0xFF .&. hi) 8 + lo
 
 wordToBytes :: Word16 -> (Word8, Word8)
 wordToBytes w = (fromIntegral . shiftR (0xF0 .&. w) $ 8, fromIntegral $ 0x0F .&. w)
+
+firstByte :: Word16 -> Word8
+firstByte = fst . wordToBytes
+
+secondByte :: Word16 -> Word8
+secondByte = snd . wordToBytes
 
 byteToNibbles :: Word8 -> (Word8, Word8)
 byteToNibbles b = (shiftR (0b11110000 .&. b) 4, 0b00001111 .&. b)
