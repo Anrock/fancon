c70ee199efbcbeb7e884ef3c40df65af9ec466cb
Anrock <anrock623@gmail.com>
2020-05-25 01:11:41 +0300

    Add Arguments module with optparse-applicative

---
+++ src/Arguments.hs
@@ -0,0 +1,33 @@
+module Arguments (CompileOptions(..), Command(..), getArguments) where
+
+import Options.Applicative
+
+data Command = Compile CompileOptions
+
+data CompileOptions = CompileOptions
+  { files :: [FilePath],
+    output :: FilePath,
+    dumpSymbolTable :: Bool
+  }
+
+compileOptionsParser :: Parser CompileOptions
+compileOptionsParser = CompileOptions
+  <$> some (argument str (metavar "INPUT"))
+  <*> strOption (
+           long "output"
+        <> short 'o'
+        <> metavar "OUTPUT"
+        <> value "out.exe.fancon"
+        <> help "File name of compiled binary")
+  <*> switch (long "dump-symbol-table" <> help "Print symbol tables when compiling")
+
+compileParser :: Parser Command
+compileParser = Compile <$> compileOptionsParser
+
+commandParser :: Parser Command
+commandParser = subparser (
+  command "compile" ( info (compileParser <**> helper) ( progDesc "Compile fancon assembly" ) )
+  )
+
+getArguments :: IO Command
+getArguments = execParser (info (commandParser <**> helper) (header "Fancon - a fantasy console" <> fullDesc))
